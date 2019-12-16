24507b48127a8d8729e6dcbae1d91d5359783791
ruhul-11 <ruhul901111@gmail.com>
2019-12-16 12:41:23 +0600

    Add Entity Framework

---
+++ University-Management-Final-Project/UniversityManagement/UniversityManagement/DataContext/UniversityDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using UniversityManagement.Models;
+
+namespace UniversityManagement.DataContext
+{
+    public class UniversityDbContext:DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+    }
+}
