bef6d5ebbf8b0c709fd57436ff464a2be9cae9de
ruhul-11 <ruhul901111@gmail.com>
2019-12-17 11:55:30 +0600

    Added all Dropdown Menu

--- University-Management-Final-Project/UniversityManagement/UniversityManagement/Models/Department.cs
+++ University-Management-Final-Project/UniversityManagement/UniversityManagement/Models/Department.cs
@@ -1,9 +1,24 @@
-﻿namespace UniversityManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace UniversityManagement.Models
 {
 	//Department Class
     public class Department
     {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Department Code is Required")]
+        [Display(Name = "Code")]
+        [StringLength(7, MinimumLength = 2, ErrorMessage = "Departmrnt Code Must be 2 to 7 characters long")]
+        //[Remote("IsDeptCodeExist", "Department", ErrorMessage = "Code already exists!")]
         public string Code { get; set; }
+
+
+        [Required(ErrorMessage = "Department Name is Required")]
+        [Display(Name = "Name")]
+        //[Remote("IsDeptNameExist", "Department", ErrorMessage = "Name already exists!")]
         public string Name { get; set; }
     }
 }
